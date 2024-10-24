302944a6876f9dc9eedce6e9951041c8fd5214af
Foyeke <you@example.com>
2024-10-24 15:28:08 -0500

    Updates to log in backend

---
+++ meet-me-half-way/src/components/firebase/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; 
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDs9dukouikGyLKxjQgTnM1s2bMQ5h_ezs",
+  authDomain: "meet-me-halfway-5475f.firebaseapp.com",
+  projectId: "meet-me-halfway-5475f",
+  storageBucket: "meet-me-halfway-5475f.appspot.com",
+  messagingSenderId: "140642671795",
+  appId: "1:140642671795:web:49943cd681cdde8e4364c6",
+  measurementId: "G-5BL66VCECJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export {app, auth};
---
+++ meet-me-half-way/src/context/authContext/index.jsx
@@ -0,0 +1,48 @@
+import {useEffect } from "react";
+import {auth} from '../../components/firebase/firebase';
+import { onAuthStateChanged } from "firebase/auth";
+
+const AuthContext = React.createContext();
+
+export function useAuth()
+{
+    return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) 
+{
+    const [currentUser, setCurrentUser] = useState(null);
+    const [userLoggedIn, setUserLoggedIn] = useState(false);
+    const [loading, setLoading] = useState(true);   
+    
+    useEffect(()=>
+    {
+        const unsubscribe = onAuthStateChanged(auth, initalizeUser);
+        return unsubscribe;
+    }, [])
+
+    async function initalizeUser(user)
+    {
+        if (user) {
+            setCurrentUser({ ...user});
+            setUserLoggedIn(true);
+        } 
+        else {
+            setCurrentUser(null);
+            setUserLoggedIn(false);
+        }
+        setLoading(false);
+    }
+
+    const value = {
+        currentUser,
+        userLoggedIn,
+        loading
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children && children}
+        </AuthContext.Provider>
+    )
+}
