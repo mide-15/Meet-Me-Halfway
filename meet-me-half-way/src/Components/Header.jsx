e919353cf71f542af0ee6b00e406087f7b8a9e0e
jcbeaudreau <jcbeaudreau@gmail.com>
2025-03-30 17:20:10 -0500

    Update Header.jsx

--- meet-me-half-way/src/Components/Header.jsx
+++ meet-me-half-way/src/Components/Header.jsx
@@ -1,14 +1,15 @@
-import { Button, Img } from "./.."
+import { Button } from "../components/ui/button"; // Adjust path as needed
+import { Img } from "../components/ui/img"; // Ensure correct path
 import React from "react";
 
-export default function Header({ ...props }) {
+export default function Header({ className, ...props }) {
   return ( 
     <header
       {...props}
-      className={${props.className} flex items-center h-[104px] md:h-auto p-5 rounded-t1-[10px] rounded-tr-[10px] bg-[url(/public/images/img_group_56.png)] bg-cover bg-no-repeat`}
+      className={`${className} flex items-center h-[104px] md:h-auto p-5 rounded-tl-[10px] rounded-tr-[10px] bg-[url(/public/images/img_group_56.png)] bg-cover bg-no-repeat`}
     >
-      <div className="mx-auto flex w-full max-w-| -w-[1376px] items-center justify-between gap-5 self-end">
-        <Img src="images/img_header_logo.svg" alt="Headerlogo" className="h-[48px] w-[48px] object-contain" /> 
+      <div className="mx-auto flex w-full max-w-[1376px] items-center justify-between gap-5 self-end">
+        <Img src="images/img_header_logo.svg" alt="Header logo" className="h-[48px] w-[48px] object-contain" /> 
         <Button shape="square" className="min-w-[168px] px-6 sm:px-5">
           Update
         </Button>
