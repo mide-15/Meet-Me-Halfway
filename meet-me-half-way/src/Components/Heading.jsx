8d8ac52fea1e66cfac349036178379d83b2aa7ab
jcbeaudreau <jcbeaudreau@gmail.com>
2025-03-30 17:20:29 -0500

    Update Heading.jsx

--- meet-me-half-way/src/Components/Heading.jsx
+++ meet-me-half-way/src/Components/Heading.jsx
@@ -3,14 +3,13 @@ import React from "react";
 const Heading = ({
   children,
   className = "",
-
   as,
   ...restProps
 }) => {
   const Component = as || "h6";
 
   return (
-    <Component className={' ${className} '}{...restProps}>
+    <Component className={`${className}`} {...restProps}>
       {children}
     </Component>
   );
