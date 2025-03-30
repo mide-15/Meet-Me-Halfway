9cec85e6e949371f1a2d7d61293330fbc27160d4
jcbeaudreau <jcbeaudreau@gmail.com>
2025-03-30 17:21:18 -0500

    Update Text.jsx

--- meet-me-half-way/src/Components/Text.jsx
+++ meet-me-half-way/src/Components/Text.jsx
@@ -5,12 +5,12 @@ const sizes = {
   texts: "text-[32px] font-normal md:text-[30px] sm:text-[28px]", 
   textmd: "text-[40px] font-normal md:text-[38px] sm:text-[36px]",
 };
-  
+
 const Text = ({ children, className = "", as, size = "textxs", ...restProps }) => {
   const Component = as || "p";
 
   return (
-    <Component className={`text-black-900_cc font-poppins ${className} ${sizes[size]} `} {.....restProps}>
+    <Component className={`text-black font-poppins ${className} ${sizes[size]}`} {...restProps}>
       {children}
     </Component>
   );
