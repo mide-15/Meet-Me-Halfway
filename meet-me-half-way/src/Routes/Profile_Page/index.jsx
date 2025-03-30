6eed176c6f5522467548732688fb9dbfdeb2460c
jcbeaudreau <jcbeaudreau@gmail.com>
2025-03-30 17:12:59 -0500

    Update index.jsx

--- meet-me-half-way/src/Routes/Profile_Page/index.jsx
+++ meet-me-half-way/src/Routes/Profile_Page/index.jsx
@@ -1,6 +1,6 @@
-import {Helmet } from "react-helmet";
-import { Button } from "../../components";
-import Profile from "./ProfileInformationSection"; 
+import { Helmet } from "react-helmet";
+import { Button } from "../../components/ui/button"; // Ensure correct path
+import ProfileInformationSection from "./ProfileInformationSection"; // Fixed import
 import React from "react";
 
 export default function ProfilePage() {
@@ -14,13 +14,13 @@ export default function ProfilePage() {
         />
       </Helmet>
       <div className="flex w-full flex-col gap-2 bg-gray-50">
-        <header className="flex h-[104px] items-center justify-end rounded-t1-[10px] rounded-tr-[10px] bg-[url(/public/images/img_group_56.png)] bg-cover bg-no-repeat px-[34px] py-5 md:h-auto sm:px-5"> 
+        <header className="flex h-[104px] items-center justify-end rounded-tl-[10px] rounded-tr-[10px] bg-[url(/public/images/img_group_56.png)] bg-cover bg-no-repeat px-[34px] py-5 md:h-auto sm:px-5"> 
           <Button shape="square" className="min-w-[168px] self-end px-[34px] sm:px-5">
             Edit
           </Button>
         </header>
         
-        {/* profile information section */}
+        {/* Profile information section */}
         <ProfileInformationSection />
       </div>
     </>
