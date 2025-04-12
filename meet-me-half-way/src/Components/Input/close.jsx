4ed9b946c89c1565967b0bfcd824ba9c49b5e30a
jcbeaudreau <jcbeaudreau@gmail.com>
2025-04-11 19:12:09 -0500

    Update close.jsx

--- meet-me-half-way/src/Components/Input/close.jsx
+++ meet-me-half-way/src/Components/Input/close.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-const closeSVG = ({ fillcolor="#000000", className = ', ...props }) => { 
+const CloseSVG = ({ fillColor = "#000000", className = "", ...props }) => {
   return (
     <svg
       fill={fillColor}
       xmlns="http://www.w3.org/2000/svg"
       className={className}
+      height={props?.height || 20}
+      width={props?.width || 20}
+      viewBox="0 0 24 24"
       {...props}
-      height={props?.width || 20}
-      width={props?.height || 20}
-      viewBox={`> > ${props?.width || 20} ${props?.height || 20} `}
-    ></svg>
-      <path d="M 4.7070312 3.2929688 L 3.2929688 4.7078312 L 10.585938 12 L 3.2929688 19.292969 L 4.7878312 20.707031 L 12 13.414062 L 19.292969 20.707831 L 20.707031 19.292969 L 13.414062 12 L 20.707831 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z" /> 
+    >
+      <path d="M4.707 3.293 3.293 4.707 10.586 12 3.293 19.293 4.707 20.707 12 13.414 19.293 20.707 20.707 19.293 13.414 12 20.707 4.707 19.293 3.293 12 10.586z" />
     </svg>
   );
 };
-      
+
 export { CloseSVG };
