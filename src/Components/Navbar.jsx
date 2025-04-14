bbbc585f858899cc092902c135d5783a7c6bdc0b
cvalerie1102 <90990858+cvalerie1102@users.noreply.github.com>
2025-04-13 20:14:10 -0500

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,11 @@
-// Placeholder Navbar component
-
 import React, { useState } from "react";
 import { signOut } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
-import { Navigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom';
 import "./NavbarStyle.css";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 
-// Firebase configuration (this should be an env var)
 const firebaseConfig = {
   apiKey: "AIzaSyDs9dukouikGyLKxjQgTnM1s2bMQ5h_ezs",
   authDomain: "meet-me-halfway-5475f.firebaseapp.com",
@@ -20,7 +17,6 @@ const firebaseConfig = {
   measurementId: "G-5BL66VCECJ"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
@@ -28,56 +24,80 @@ const auth = getAuth(app);
 const Navbar = () => {
   const [shouldRedirect, setShouldRedirect] = useState(false);
 
-  // Call firebase api to delete auth token and set redirect flag
-  const handleLogOut = (event) => {
+  const handleLogOut = () => {
     signOut(auth)
       .then(() => {
-        // clear storage and set flag
         localStorage.clear();
         setShouldRedirect(true);
       })
       .catch((error) => {
-        console.log(error);
+        console.error(error);
       });
-  }
+  };
 
   return (
-    <nav class="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "#031749" }}>
-      <a class="navbar-brand" href="#">Logo</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "#031749" }}>
+      <a className="navbar-brand" href="/">
+        <video
+          src={`${process.env.PUBLIC_URL}/hmlogovid2.mov`}
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="placeholder-lightbox"
+          style={{ width: '50px', height: 'auto' }}
+        />
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link text-white" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link text-white" href="/login">Login</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link text-white" href="/registration">Register</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link text-white" href="/dashboard">Map</a>
-        </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Settings
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link text-white" href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="/login">Login</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="/registration">Register</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="/dashboard">Map</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle text-white"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Settings
             </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Account</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" type="submit" onClick={handleLogOut}>Sign Out</a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item" href="#">Account</a>
+              <a className="dropdown-item" href="#">Another action</a>
+              <div className="dropdown-divider"></div>
+              <button className="dropdown-item" onClick={handleLogOut}>Sign Out</button>
             </div>
-        </li>
+          </li>
         </ul>
       </div>
       {shouldRedirect && <Navigate to="/" />}
-      </nav>
-  )
-}
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
